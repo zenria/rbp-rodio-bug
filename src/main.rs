5b2c0ff923da6c1c2c0958bfa47b8010fe610a5b
Philippe GASSMANN <philippe.gassmann@linkfluence.com>
2020-11-05 10:38:08 +0100

    logs & release profile

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use cpal::traits::{DeviceTrait, HostTrait};
-use env_logger::Env;
+use log::*;
 use rodio::{OutputStream, Sink};
 use std::io::Cursor;
 use std::time::Duration;
@@ -7,7 +7,7 @@ use std::time::Duration;
 const ALARME: &[u8; 578801] = include_bytes!("alarme.wav");
 
 fn main() {
-    env_logger::Builder::from_env(Env::default()).init();
+    env_logger::init();
     let device = cpal::default_host()
         .output_devices()
         .expect("Unable to list output devices of default host")
@@ -20,11 +20,13 @@ fn main() {
         })
         .or_else(|| cpal::default_host().default_output_device())
         .expect("Unable to initialize sound system");
-    println!("Using sound output device: {}", device.name().unwrap());
+    info!("Using sound output device: {}", device.name().unwrap());
     let (_output_stream, output_stream_handle) = OutputStream::try_from_device(&device)
         .expect("Unable to open outputstream to sound device!");
     let sink = Sink::try_new(&output_stream_handle).expect("Unable to create sink");
+    info!("Sink created, start playback");
     sink.append(rodio::Decoder::new(Cursor::new(ALARME.to_vec())).unwrap());
-
+    info!("Wait for 5 seconds...");
     std::thread::sleep(Duration::from_secs(5));
+    info!("Quit!");
 }
